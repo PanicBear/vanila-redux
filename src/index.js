8eec97f01659fa402c799d251b1f76102b7a8933
ClarXo <cheonaru@gmail.com>
2022-04-11 23:38:25 +0900

    todo app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,63 @@
+const { createStore } = require('redux');
+
+const store = createStore(reducer);
+
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
 
-const createToDo = (toDo) => {
-  const li = document.createElement('li');
-  li.innerText = toDo;
-  ul.appendChild(li);
+function reducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD':
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case 'DELETE':
+      return state.filter(({ id }) => id !== action.id);
+    default:
+      return state;
+  }
+}
+
+const addTodo = (text) => {
+  return {
+    type: 'ADD',
+    text,
+  };
+};
+const deleteTodo = (id) => {
+  return {
+    type: 'DELETE',
+    id,
+  };
 };
 
-const onSubmit = (e) => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = '';
-  createToDo(toDo);
+const dispatchAddTodo = (todo) => store.dispatch(addTodo(todo));
+
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentElement.id);
+  store.dispatch(deleteTodo(id));
 };
+
+const paintTodos = () => {
+  const todos = store.getState();
+  ul.innerHTML = '';
+  todos.forEach(({ id, text }) => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.addEventListener('click', dispatchDeleteTodo);
+    btn.innerText = 'DEL';
+    li.id = id;
+    li.innerText = text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+function onSubmit(e) {
+  e.preventDefault();
+  const todo = input.value;
+  input.value = '';
+  dispatchAddTodo(todo);
+}
 
 form.addEventListener('submit', onSubmit);
+store.subscribe(paintTodos);
