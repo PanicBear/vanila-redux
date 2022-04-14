5c452c445cd0135910e208b05f63d7ceff5a1668
ClarXo <cheonaru@gmail.com>
2022-04-15 08:36:24 +0900

    get id with useParams

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { actionCreators } from '../store';
 
-function Todo({ text, onClick }) {
+function Todo({ text, onClick, id }) {
   return (
     <li>
-      {text}
-      <button onClick={onClick}>DEL</button>
+      <Link to={`/${id}`}>
+        {text}
+        <button onClick={onClick}>DEL</button>
+      </Link>
     </li>
   );
 }
--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,1 +1,20 @@
-export default () => 'Detail';
+import React from 'react';
+import { connect } from 'react-redux';
+import { useParams } from 'react-router';
+
+function Detail({ todos }) {
+  const { id } = useParams();
+  const { text } = todos.find((todo) => todo.id === parseInt(id));
+  return (
+    <>
+      <h1>Detail</h1>
+      <h5>Created at: {text}</h5>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  return { todos: state };
+}
+
+export default connect(mapStateToProps)(Detail);
