07ff885bcbd814b9926fd58e4d20c3e07ab28ebf
ClarXo <cheonaru@gmail.com>
2022-04-14 11:35:36 +0900

    add dispatch to props with mapDisPatchToProps

--- src/store.js
+++ src/store.js
@@ -3,12 +3,26 @@ import { createStore } from 'redux';
 const ADD = 'ADD';
 const DELETE = 'DELETE';
 
+const addTodo = (text) => {
+  return {
+    type: ADD,
+    text,
+  };
+};
+
+const deleteTodo = (id) => {
+  return {
+    type: DELETE,
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD:
       return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE:
-      return state.filter((todo) => todo !== action.id);
+      return state.filter((todo) => todo.id !== action.id);
     default:
       return state;
   }
@@ -16,6 +30,9 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
-store.subscribe();
+export const actionCreators = {
+  addTodo,
+  deleteTodo,
+};
 
 export default store;
